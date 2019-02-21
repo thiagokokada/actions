c7198bc6e075f562b3d22fd2eb160987c5a8474d
Thiago Kenji Okada (Nubank) <thiago.okada@nubank.com.br>
2019-02-21 17:20:59 -0300

    Ignore project.clj file while running nsorg

--- nsorg/entrypoint.sh
+++ nsorg/entrypoint.sh
@@ -6,8 +6,11 @@ set -euo pipefail
 source /lib.sh
 
 nsorg() {
+  # Ignore project.clj file since this would make lein to download project deps
+  mv project.clj project.clj.bkp
   clojure -Sdeps '{:deps {leiningen {:mvn/version "2.9.0"} lein-nsorg {:mvn/version "0.2.0"}}}' \
     -m leiningen.core.main nsorg "${@}"
+  mv project.clj.bkp project.clj
 }
 
 fix() {
